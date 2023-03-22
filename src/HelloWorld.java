fd0eb88f797cb967c87367d7a005128b64b173c3
Epson <A.osinin.epson@gmail.com>
2023-03-22 22:02:00 +0300

    int commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,53 @@
+public class HelloWorld {
+//    //комментарии
+//    // public - видят все, static - не нужен объект нашего класса,
+//    //void - ничего нам не возращает
+//    //main - говорит что это старт нашего приложения
+//    //psvm - что бы не писать руками
+//
+   public static void main(String[] args) {
+//        byte buteExample = 3;
+//        byte secondByte = (byte) myShort;
+//        char letter = 'a';
+//        int myLetter = 30;
+//        int c = 3;
+//        int b = 5;
+//        float myFloatNumber = 2.5f;
+//        double myDouble = 3.5;
+//
+//        boolean isTrue = true;
+//        boolean isFalse = false;
+//
+//        System.out.println(isTrue);
+//        System.out.println(myLetter);
+//
+//        // +, -, *, /
+//        int aa = 4;
+//        int bb = 3;
+//        System.out.println((double) aa/bb);
+//        System.out.println(double(aa/bb));
+//        System.out.println();
+//        ;
+//
+//
+//        //sout - not to write by hands
+        System.out.println("Hello world!!!");
+        int minValue = 3;
+        int maxValue = 5;
+       System.out.println(minValue> 0 && maxValue >10);
+       System.out.println(minValue> 0 || minValue > 10);
+       System.out.println(!(minValue> 0));
+
+       minValue +=3;
+       minValue -=3;
+       minValue *=3;
+       minValue /=3;
+       minValue %=3;
+
+
+
+
+
+
+    }
+}
