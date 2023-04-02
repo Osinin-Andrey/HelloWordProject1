84765c700addd8738b48151b0f4d0b7f10f3c1ca
Epson <A.osinin.epson@gmail.com>
2023-04-02 22:47:58 +0300

    Homework3_1

---
+++ src/by/osinin/homework/lesson3/Homework3_3.java
@@ -0,0 +1,12 @@
+package by.osinin.homework.lesson3;
+
+public class Homework3_3 {
+    public static void main(String[] args){
+        int x=7;
+        while (x<100) {
+            System.out.print(x+" ");
+            x=x+7;
+        }
+
+    }
+}
