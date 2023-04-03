1d1b7b95fa34f044bbca2fba1af67078ff239543
Epson <A.osinin.epson@gmail.com>
2023-04-03 22:02:42 +0300

    classwork5

---
+++ src/by/osinin/homework/lesson4/Homework4_2.java
@@ -0,0 +1,14 @@
+package by.osinin.homework.lesson4;
+
+import java.util.Random;
+
+public class Homework4_2 {
+    public static void main(String[] args) {
+        int[] arr = new int [10];
+        Random random = new Random();
+
+
+
+
+    }
+}
