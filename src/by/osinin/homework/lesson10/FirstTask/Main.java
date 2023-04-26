a307be2ab0779c460149b43202803778dadb3514
Epson <A.osinin.epson@gmail.com>
2023-04-26 16:47:46 +0300

    HomeWork 10, add print

--- src/by/osinin/homework/lesson10/FirstTask/Main.java
+++ src/by/osinin/homework/lesson10/FirstTask/Main.java
@@ -11,16 +11,19 @@ public class Main {
         //System.out.println(Arrays.toString(arr));
         System.out.println(arr[0] + " " + arr[2]);
         //Вывести на экран в одну строку два первых блока по 4 цифры.
-        lettersSymbols(document);
-        lettersSymbolsTwo(document);
-        lettersToLowerCase(document);
-        lettersToUpperCase(document);
-        lettersAbc(document);
-        numbers555(document);
-        symbol1a2b(document);
+        printLettersSymbols(document);
+        printLettersSymbolsTwo(document);
+        printLettersToLowerCase(document);
+        printLettersToLowerCase(document);
+        printLettersToUpperCase(document);
+        printLettersAbc(document);
+        printNumbers555(document);
+        printSymbol1a2b(document);
+        printLettersSymbolsThree(document);
+        printLettersToLowerCaseTwo(document);
     }
 
-    private static void lettersSymbols(String document) {
+    private static void printLettersSymbols(String document) {
         StringBuilder builder = new StringBuilder(document);
         builder.replace(5, 8, "***");
         System.out.println(builder.replace(14, 17, "***"));
@@ -28,14 +31,15 @@ public class Main {
         // заменить на *** (каждая буква заменятся на *).
     }
 
-    private static void lettersSymbolsTwo(String document) {
+    private static void printLettersSymbolsTwo(String document) {
         System.out.println(document.replaceAll("[a-zA-Z]{3}", "***"));
         //или такой способ нашёл в интернете, самый удобный по идее
         // Вывести на экран номер документа, но блоки из трех букв
         // заменить на *** (каждая буква заменятся на *).
     }
 
-    private static void lettersToLowerCase(String document) {
+
+    private static void printLettersToLowerCase(String document) {
         System.out.println(document.replaceAll("[0-9]{4}", "+").
                 replaceAll("[0-9-+]+{1}", "/").
                 replaceFirst("/", "").
@@ -44,7 +48,7 @@ public class Main {
         //документа в формате yyy/yyy/y/y в нижнем регистре.
     }
 
-    private static void lettersToUpperCase(String document) {
+    private static void printLettersToUpperCase(String document) {
         StringBuilder builder = new StringBuilder(document);
         System.out.println("Letters:" + builder.delete(0, 5).
                 replace(3, 9, "/").
@@ -56,7 +60,7 @@ public class Main {
         //с помощью класса StringBuilder).
     }
 
-    private static void lettersAbc(String document) {
+    private static void printLettersAbc(String document) {
         String LettersAbc = "ABC";
         if (document.contains(LettersAbc.toLowerCase()))
             System.out.println("Document has: " + LettersAbc.toLowerCase());
@@ -70,17 +74,17 @@ public class Main {
         }
     }
 
-    private static void numbers555 (String document) {
+    private static void printNumbers555(String document) {
         String numbers555 = "555";
         if (document.startsWith(numbers555))
-        System.out.println("Document has numbers 555 at first");
+            System.out.println("Document has numbers 555 at first");
         else {
             System.out.println("Document does not has numbers 555 at first");
         }
         //Проверить начинается ли номер документа с последовательности 555.
     }
 
-    private static void symbol1a2b (String document) {
+    private static void printSymbol1a2b(String document) {
         String symbol1a2b = "1a2b";
         if (document.endsWith(symbol1a2b))
             System.out.println("Document has symbol 1a2b at end");
@@ -89,5 +93,19 @@ public class Main {
         }
         //Проверить заканчивается ли номер документа на последовательность 1a2b.
     }
+
+    private static void printLettersSymbolsThree(String document) {
+        System.out.println(document.substring(0, 5) + "***" + document.substring(8, 14) + "***");
+        // Вывести на экран номер документа, но блоки из трех букв
+        // заменить на *** (каждая буква заменятся на *).
+    }
+
+    private static void printLettersToLowerCaseTwo(String document) {
+        System.out.println((document.substring(5, 8) + "/" + document.substring(14, 17) +"/"
+                +document.substring(19,20)+ "/" + document.substring(21,22)).toLowerCase());
+        //Вывести на экран только одни буквы из номера
+        //документа в формате yyy/yyy/y/y в нижнем регистре.
+    }
+
 
 }
---
+++ src/by/osinin/homework/lesson10/SecondTask/Main.java
@@ -0,0 +1,7 @@
+package by.osinin.homework.lesson10.SecondTask;
+
+public class Main {
+    public static void main (String[] args){
+
+    }
+}
