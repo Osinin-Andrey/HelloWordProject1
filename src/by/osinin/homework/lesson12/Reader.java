050c8082d8ff8cda8a3f626d67f08fc7819e4dee
Epson <A.osinin.epson@gmail.com>
2023-05-10 17:30:51 +0300

    Homework 12.1

--- src/by/osinin/homework/lesson12/Reader.java
+++ src/by/osinin/homework/lesson12/Reader.java
@@ -6,16 +6,19 @@ import java.util.Scanner;
 
 public class Reader {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter way to file: ");
+        String file = scanner.nextLine();
 
 
-        try (FileReader reader = new FileReader("C:\\Users\\Epson\\IdeaProjects\\HelloWordProject\\out2\\document.txt");
-             Scanner scanner = new Scanner(reader);
+        try (FileReader reader = new FileReader(file);
+             Scanner fileScanner = new Scanner(reader);
              FileWriter writer = new FileWriter("C:\\Users\\Epson\\IdeaProjects\\HelloWordProject\\out2\\results.txt");
              FileWriter writer1 = new FileWriter("C:\\Users\\Epson\\IdeaProjects\\HelloWordProject\\out2\\failed_results.txt")) {
 
 
-            while (scanner.hasNext()) {
-                String str = scanner.nextLine();
+            while (fileScanner.hasNext()) {
+                String str = fileScanner.nextLine();
 
                 if (str.length() >= 15 && (str.startsWith("docnum") || str.startsWith("contract"))) {
                     writer.write(str +"\n");
