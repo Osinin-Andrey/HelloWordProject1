306d504a49676c8f5a804ee351d0d079c507e3a1
Epson <A.osinin.epson@gmail.com>
2023-05-16 16:43:59 +0300

    Howework 13.2

--- src/by/osinin/homework/lesson13/task2/Main.java
+++ src/by/osinin/homework/lesson13/task2/Main.java
@@ -4,16 +4,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         try (FileReader reader = new FileReader("C:\\Users\\Epson\\IdeaProjects\\HelloWordProject\\out2\\numbers.txt");
              Scanner scanner = new Scanner(reader)) {
 
-            while (scanner.hasNext()) { //пока в этом файле что-то есть, читай эти строчки
-                System.out.println(scanner.nextLine());
-
+            Set<Integer> numbers = new HashSet<>(); // создаем HashSet для хранения чисел
+            while (scanner.hasNextInt()) { //возвращает true если с потока ввода можно считать целое число
+                numbers.add(scanner.nextInt()); //считывает целое число с потока ввода
+            }
+            for (int number : numbers) {
+                System.out.println(numbers);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
