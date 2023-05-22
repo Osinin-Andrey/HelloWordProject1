ca80d59680ec2c464f71d6c3b1c9cc433271d316
Epson <A.osinin.epson@gmail.com>
2023-05-22 22:04:01 +0300

    classwork 16

--- src/by/osinin/classwork/lesson14/MyBestInterface.java
+++ src/by/osinin/classwork/lesson14/MyBestInterface.java
@@ -2,7 +2,7 @@ package by.osinin.classwork.lesson14;
 
 @FunctionalInterface //говорим что будет 1 интерфеис
 public interface MyBestInterface {
-    void test();
+    String test(String a);
     //void test2(); больше ничего не добавить
 
 
---
+++ src/by/osinin/classwork/lesson16/User.java
@@ -0,0 +1,30 @@
+package by.osinin.classwork.lesson16;
+
+public class User {
+    private int age;
+    private String name;
+
+    public User() {
+    }
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
